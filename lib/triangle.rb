b564c5917979bf9d403cf1ba8d82ad18e7317a9c
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-09-13 13:11:17 +1000

    commit tree 37d3b5f6affbb5886e9bbf45357beac1a11f0d03

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+    validate_triangle
+  end
+
+  def kind
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  class TriangleError < StandardError
+  end  
+
+  def validate_triangle
+    sides = [@side1, @side2, @side3].sort
+    if sides.any? { |side| side <= 0 } || sides[0] + sides[1] <= sides[2]
+      raise TriangleError, "Invalid triangle sides"
+    end
+  end
 end
